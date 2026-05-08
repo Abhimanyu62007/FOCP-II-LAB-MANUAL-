f34304aaf99836d45607ee14ddf3835325f4bfa1
Abhimanyu Sharma <abhimsharma62007@gmail.com>
2026-05-08 21:23:13 +0530

    Add character classification program in C

---
+++ q9.cpp
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <ctype.h>
+
+int main() {
+    char ch;
+
+    scanf("%c", &ch);
+
+    if(isdigit(ch))
+        printf("Number");
+    else if(ch=='a'||ch=='e'||ch=='i'||ch=='o'||ch=='u'||
+            ch=='A'||ch=='E'||ch=='I'||ch=='O'||ch=='U')
+        printf("Vowel");
+    else if(isalpha(ch))
+        printf("Consonant");
+    else
+        printf("Special Character");
+
+    return 0;
+}
