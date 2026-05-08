56d7013c3301c518149df8f9d3a87912918b19d9
Abhimanyu Sharma <abhimsharma62007@gmail.com>
2026-05-08 21:22:48 +0530

    Implement BuzzFuzz program in C

---
+++ q8.cpp
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+int main() {
+    int n, i;
+
+    scanf("%d", &n);
+
+    for(i = 1; i <= n; i++) {
+        if(i % 3 == 0 && i % 5 == 0)
+            printf("BuzzFuzz\n");
+        else if(i % 3 == 0)
+            printf("Buzz\n");
+        else if(i % 5 == 0)
+            printf("Fuzz\n");
+        else
+            printf("%d\n", i);
+    }
+
+    return 0;
+}
